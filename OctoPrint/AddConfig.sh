669eab3cc2b0a396fd391f5e82bf625bdd776c9a
Pontiac76 <pontiac76@gmail.com>
2022-01-22 00:36:13 -0500

    Create AddConfig.sh

---
+++ OctoPrint/AddConfig.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#
+# This script will attempt to copy the Controls YAML file into your OctoPrint main configuration.
+#
+# First, install the CustomControl module inside OctoPrint or go to https://github.com/Salandora/octoprint-customControl and do a manual install.
+#
+# Next log into your OctoPrint directory and run this:
+#
+# 
